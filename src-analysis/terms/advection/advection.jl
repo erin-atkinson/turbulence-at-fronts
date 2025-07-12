b5f48574a35afca892d09d5ba28f17bb9319be19
Erin Atkinson <erin.atkinson60@gmail.com>
2025-07-12 18:02:39 +0100

    Fix signature error in `div_𝐯u′, v, w`

--- src-analysis/terms/advection/advection.jl
+++ src-analysis/terms/advection/advection.jl
@@ -52,6 +52,6 @@ using Oceananigans.Advection: advective_momentum_flux_Uu,
 @inline ∂Wc∂z_func(i, j, k, grid, advection, W, c) = @inbounds δzᵃᵃᶜ(i, j, k, grid, advective_tracer_flux_z, advection, W, c) / Vᶜᶜᶜ(i, j, k, grid)
 
 
-@inline div_𝐯u′(i, j, k, grid, advection, u, u_dfm) = div_𝐯u(i, j, k, grid, advection, total_velocities, u) - div_𝐯u(i, j, k, grid, advection, total_velocities, u_dfm)
-@inline div_𝐯v′(i, j, k, grid, advection, v, v_dfm) = div_𝐯v(i, j, k, grid, advection, total_velocities, v) - div_𝐯v(i, j, k, grid, advection, total_velocities, v_dfm)
-@inline div_𝐯w′(i, j, k, grid, advection, w, w_dfm) = div_𝐯w(i, j, k, grid, advection, total_velocities, w) - div_𝐯w(i, j, k, grid, advection, total_velocities, w_dfm)
+@inline div_𝐯u′(i, j, k, grid, advection, total_velocities, u, u_dfm) = div_𝐯u(i, j, k, grid, advection, total_velocities, u) - div_𝐯u(i, j, k, grid, advection, total_velocities, u_dfm)
+@inline div_𝐯v′(i, j, k, grid, advection, total_velocities, v, v_dfm) = div_𝐯v(i, j, k, grid, advection, total_velocities, v) - div_𝐯v(i, j, k, grid, advection, total_velocities, v_dfm)
+@inline div_𝐯w′(i, j, k, grid, advection, total_velocities, w, w_dfm) = div_𝐯w(i, j, k, grid, advection, total_velocities, w) - div_𝐯w(i, j, k, grid, advection, total_velocities, w_dfm)
