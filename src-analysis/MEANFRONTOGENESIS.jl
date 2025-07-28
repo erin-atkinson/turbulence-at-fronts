9acba20dd988a49dc90a38680beb89620bc6e862
Erin Atkinson <erin.atkinson60@gmail.com>
2025-07-28 15:16:46 +0100

    Change mean frontogenesis equation to dbdx rather than square

--- src-analysis/MEANFRONTOGENESIS.jl
+++ src-analysis/MEANFRONTOGENESIS.jl
@@ -19,49 +19,54 @@ b_dfm = dfm(fields.b)
 
 mean_fields = (; u_dfm, v_dfm, w_dfm, b_dfm)
 
-∇b² = Field(KernelFunctionOperation{Center, Center, Center}(∇b²_func, grid, clock, fields, mean_fields, sp))
+dbdx = Field(KernelFunctionOperation{Center, Nothing, Center}(dbdx_func, grid, clock, fields, mean_fields, sp))
 
 # Resolved terms in frontogenesis equation
-background_strain = Field(KernelFunctionOperation{Center, Center, Center}(background_strain_func, grid, clock, fields, mean_fields, sp))
-divergence = Field(KernelFunctionOperation{Center, Center, Center}(divergence_func, grid, clock, fields, mean_fields, sp))
-tilting = Field(KernelFunctionOperation{Center, Center, Center}(tilting_func, grid, clock, fields, mean_fields, sp))
+background_strain = Field(KernelFunctionOperation{Center, Nothing, Center}(background_strain_func, grid, clock, fields, mean_fields, sp))
+divergence = Field(KernelFunctionOperation{Center, Nothing, Center}(divergence_func, grid, clock, fields, mean_fields, sp))
+tilting = Field(KernelFunctionOperation{Center, Nothing, Center}(tilting_func, grid, clock, fields, mean_fields, sp))
 
 # Turbulence
 turbulence_h = Field(KernelFunctionOperation{Center, Center, Center}(turbulence_h_func, grid, clock, fields, mean_fields, sp))
 turbulence_z = Field(KernelFunctionOperation{Center, Center, Center}(turbulence_z_func, grid, clock, fields, mean_fields, sp))
 
+# Advection
+adv_x = Field(KernelFunctionOperation{Center, Center, Center}(adv_x_func, grid, clock, fields, (; dbdx), sp))
+adv_y = Field(KernelFunctionOperation{Center, Center, Center}(adv_y_func, grid, clock, fields, (; dbdx), sp))
+adv_z = Field(KernelFunctionOperation{Center, Center, Center}(adv_z_func, grid, clock, fields, (; dbdx), sp))
+
 # Subgrid terms
-subgrid = Field(KernelFunctionOperation{Center, Center, Center}(subgrid_func, grid, clock, fields, mean_fields, sp))
+# subgrid = Field(KernelFunctionOperation{Center, Center, Center}(subgrid_func, grid, clock, fields, mean_fields, sp))
 
 frontogenesis_fields = (;
-    ∇b², 
+    dbdx, 
     background_strain,
     divergence,
-    tilting,
+    tilting
+)
+
+frontogenesis_fields_3D = (;
     turbulence_h,
     turbulence_z,
-    subgrid
+    adv_x,
+    adv_y,
+    adv_z
 )
 
-(
-    ∇b²_dfm, 
-    background_strain_dfm,
-    divergence_dfm,
-    tilting_dfm,
-    turbulence_h_dfm,
-    turbulence_z_dfm,
-    subgrid_dfm
-) = map(dfm, frontogenesis_fields)
+turbulence_h_dfm = dfm(turbulence_h)
+turbulence_z_dfm = dfm(turbulence_z)
+
+adv_x_dfm = dfm(adv_x)
+adv_y_dfm = dfm(adv_y)
+adv_z_dfm = dfm(adv_z)
 
 frontogenesis_fields_dfm = (;
-    ∇b²_dfm, 
-    background_strain_dfm,
-    divergence_dfm,
-    tilting_dfm,
     turbulence_h_dfm,
     turbulence_z_dfm,
-    subgrid_dfm
+    adv_x_dfm,
+    adv_y_dfm,
+    adv_z_dfm
 )
 
-dependency_fields = merge(mean_fields, frontogenesis_fields, frontogenesis_fields_dfm)
-output_fields = frontogenesis_fields_dfm
+dependency_fields = merge(mean_fields, frontogenesis_fields, frontogenesis_fields_3D, frontogenesis_fields_dfm)
+output_fields = merge(frontogenesis_fields, frontogenesis_fields_dfm)
--- src-analysis/terms/meanfrontogenesis/turbulence.jl
+++ src-analysis/terms/meanfrontogenesis/turbulence.jl
@@ -1,38 +1,5 @@
 # Turbulence acting on front strength
 
-@inline function turbulence_func(i, j, k, grid, clock, fields, dependency_fields, sp)
-
-    u = fields.u
-    v = fields.v
-    w = fields.w
-    b = fields.b
-
-    u_dfm = dependency_fields.u_dfm
-    v_dfm = dependency_fields.v_dfm
-    w_dfm = dependency_fields.w_dfm
-    b_dfm = dependency_fields.b_dfm
-
-    U = fields.U
-    V = fields.V
-    W = fields.W
-
-    total_velocities = (; u = SumOfArrays{2}(u, U),
-                        v = SumOfArrays{2}(v, V),
-                        w = SumOfArrays{2}(w, W))
-    
-    mean_velocities = (; u = SumOfArrays{2}(u_dfm, U),
-                        v = SumOfArrays{2}(v_dfm, V),
-                        w = SumOfArrays{2}(w_dfm, W))
-
-    Fbx = ℑxᶜᵃᵃ(i, j, k, grid, ∂xᶠᶜᶜ, div_Uc, centered, total_velocities, b) - ℑxᶜᵃᵃ(i, j, k, grid, ∂xᶠᶜᶜ, div_Uc, centered, total_velocities, b_dfm)
-    Fby = ℑyᵃᶜᵃ(i, j, k, grid, ∂yᶜᶠᶜ, div_Uc, centered, total_velocities, b) - ℑyᵃᶜᵃ(i, j, k, grid, ∂yᶜᶠᶜ, div_Uc, centered, total_velocities, b_dfm)
-    
-    bx = ℑxᶜᵃᵃ(i, j, k, grid, ∂xᶠᶜᶜ, b_dfm)
-    by = ℑyᵃᶜᵃ(i, j, k, grid, ∂yᶜᶠᶜ, b_dfm)
-    
-    return -(bx * Fbx + by * Fby)
-end
-
 @inline function turbulence_h_func(i, j, k, grid, clock, fields, dependency_fields, sp)
 
     u = fields.u
@@ -57,16 +24,10 @@ end
                         v = SumOfArrays{2}(v_dfm, V),
                         w = SumOfArrays{2}(w_dfm, W))
 
-    Fbx_x = ℑxᶜᵃᵃ(i, j, k, grid, ∂xᶠᶜᶜ, ∂Uc∂x_func, centered, total_velocities.u, b) - ℑxᶜᵃᵃ(i, j, k, grid, ∂xᶠᶜᶜ, ∂Uc∂x_func, centered, total_velocities.u, b_dfm)
-    Fby_x = ℑyᵃᶜᵃ(i, j, k, grid, ∂yᶜᶠᶜ, ∂Uc∂x_func, centered, total_velocities.u, b) - ℑyᵃᶜᵃ(i, j, k, grid, ∂yᶜᶠᶜ, ∂Uc∂x_func, centered, total_velocities.u, b_dfm)
-
-    Fbx_y = ℑxᶜᵃᵃ(i, j, k, grid, ∂xᶠᶜᶜ, ∂Vc∂y_func, centered, total_velocities.v, b) - ℑxᶜᵃᵃ(i, j, k, grid, ∂xᶠᶜᶜ, ∂Vc∂y_func, centered, total_velocities.v, b_dfm)
-    Fby_y = ℑyᵃᶜᵃ(i, j, k, grid, ∂yᶜᶠᶜ, ∂Vc∂y_func, centered, total_velocities.v, b) - ℑyᵃᶜᵃ(i, j, k, grid, ∂yᶜᶠᶜ, ∂Vc∂y_func, centered, total_velocities.v, b_dfm)
+    Fbx_x = ℑxᶜᵃᵃ(i, j, k, grid, ∂xᶠᶜᶜ, ∂Uc∂x_func, weno, total_velocities.u, b) - ℑxᶜᵃᵃ(i, j, k, grid, ∂xᶠᶜᶜ, ∂Uc∂x_func, weno, total_velocities.u, b_dfm)
+    Fbx_y = ℑxᶜᵃᵃ(i, j, k, grid, ∂xᶠᶜᶜ, ∂Vc∂y_func, weno, total_velocities.v, b) - ℑxᶜᵃᵃ(i, j, k, grid, ∂xᶠᶜᶜ, ∂Vc∂y_func, weno, total_velocities.v, b_dfm)
     
-    bx = ℑxᶜᵃᵃ(i, j, k, grid, ∂xᶠᶜᶜ, b_dfm)
-    by = ℑyᵃᶜᵃ(i, j, k, grid, ∂yᶜᶠᶜ, b_dfm)
-    
-    return -(bx * (Fbx_x + Fbx_y) + by * (Fby_x + Fby_y))
+    return -(Fbx_x + Fbx_y)
 end
 
 @inline function turbulence_z_func(i, j, k, grid, clock, fields, dependency_fields, sp)
@@ -76,9 +37,6 @@ end
     w = fields.w
     b = fields.b
 
-    u_dfm = dependency_fields.u_dfm
-    v_dfm = dependency_fields.v_dfm
-    w_dfm = dependency_fields.w_dfm
     b_dfm = dependency_fields.b_dfm
 
     U = fields.U
@@ -88,18 +46,10 @@ end
     total_velocities = (; u = SumOfArrays{2}(u, U),
                         v = SumOfArrays{2}(v, V),
                         w = SumOfArrays{2}(w, W))
-    
-    mean_velocities = (; u = SumOfArrays{2}(u_dfm, U),
-                        v = SumOfArrays{2}(v_dfm, V),
-                        w = SumOfArrays{2}(w_dfm, W))
 
-    Fbx = ℑxᶜᵃᵃ(i, j, k, grid, ∂xᶠᶜᶜ, ∂Wc∂z_func, centered, total_velocities.w, b) - ℑxᶜᵃᵃ(i, j, k, grid, ∂xᶠᶜᶜ, ∂Wc∂z_func, centered, total_velocities.w, b_dfm)
-    Fby = ℑyᵃᶜᵃ(i, j, k, grid, ∂yᶜᶠᶜ, ∂Wc∂z_func, centered, total_velocities.w, b) - ℑyᵃᶜᵃ(i, j, k, grid, ∂yᶜᶠᶜ, ∂Wc∂z_func, centered, total_velocities.w, b_dfm)
+    Fbx = ℑxᶜᵃᵃ(i, j, k, grid, ∂xᶠᶜᶜ, ∂Wc∂z_func, weno, total_velocities.w, b) - ℑxᶜᵃᵃ(i, j, k, grid, ∂xᶠᶜᶜ, ∂Wc∂z_func, weno, total_velocities.w, b_dfm)
     
-    bx = ℑxᶜᵃᵃ(i, j, k, grid, ∂xᶠᶜᶜ, b_dfm)
-    by = ℑyᵃᶜᵃ(i, j, k, grid, ∂yᶜᶠᶜ, b_dfm)
-    
-    return -(bx * Fbx + by * Fby)
+    return -Fbx
 end
 
 turbulence_dependencies = (:u_dfm, :v_dfm, :w_dfm, :b_dfm, )
