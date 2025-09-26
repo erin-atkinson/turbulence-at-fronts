b67a78334d6e9f559f4fd657000d626f7276df05
Erin Atkinson <erin.atkinson@mail.utoronto.ca>
2025-09-26 15:04:09 -0400

    Configure for Trillium

--- jobs-simulation/Init.sh
+++ jobs-simulation/Init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --gpus-per-node=1
-#SBATCH --time=6:00:00
+#SBATCH --time=12:00:00
 #SBATCH --job-name=Init
 #SBATCH --output=../scratch/logs/Init-%j.txt
 
@@ -12,20 +12,19 @@ export JULIA_SCRATCH_TRACK_ACCESS=0
 cd ~/turbulence-at-fronts
 
 output_path=../scratch/turbulence-at-fronts/Strain
-run_time="4e5"
+run_time="2e5"
 f="1e-4"
 H="100"
 Nx=1024
 Ny=128
 Nz=256
-Ro="0.1"
+Ro="0.4"
 Ri="2.0"
 alpha="1e-5"
 Q="100"
 c="0.5"
 start_time="-4e5"
 save_time="1e3"
-s="1.02" #1.0173
+s="1.05"
 
-julia -t 24 -- src-simulation/simulation.jl $output_path $run_time $f $H $Nx $Ny $Nz $Ro $Ri $alpha $Q $c $start_time $save_time $s "Trillium GPU test"
-
+julia -t 24 -- src-simulation/simulation.jl $output_path $run_time $f $H $Nx $Ny $Nz $Ro $Ri $alpha $Q $c $start_time $save_time $s "Trillium GPU init"
--- jobs-simulation/Strain.sh
+++ jobs-simulation/Strain.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --gpus-per-node=1
-#SBATCH --time=24:00:00
+#SBATCH --time=12:00:00
 #SBATCH --job-name=Strain
 #SBATCH --output=../scratch/logs/Strain-%j.txt
 
@@ -12,7 +12,7 @@ export JULIA_SCRATCH_TRACK_ACCESS=0
 cd ~/turbulence-at-fronts
 
 output_path=../scratch/turbulence-at-fronts/Strain
-run_time="1e5"
+run_time="2e5"
 f="1e-4"
 H="100"
 Nx=1024
@@ -27,4 +27,4 @@ start_time="-4e5"
 save_time="1e3"
 s="1.05"
 
-julia -t 24 -- src-simulation/simulation.jl $output_path $run_time $f $H $Nx $Ny $Nz $Ro $Ri $alpha $Q $c $start_time $save_time $s "Larger, mist"
+julia -t 24 -- src-simulation/simulation.jl $output_path $run_time $f $H $Nx $Ny $Nz $Ro $Ri $alpha $Q $c $start_time $save_time $s "Trillium GPU run"
