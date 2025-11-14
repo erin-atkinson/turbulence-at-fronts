ee38a29f0f0480d26f00eeffbc80702b6d29c1d7
Erin Atkinson <erin.atkinson@mail.utoronto.ca>
2025-11-14 16:59:21 -0500

    Move depots inside

--- setup/setup-trig.sh
+++ setup/setup-trig.sh
@@ -2,6 +2,6 @@
 # Run this in a login node to install Julia for Trillium (GPU)
 
 module load julia/1.10.10
-export JULIA_DEPOT_PATH=$HOME/.julia-trig
+export JULIA_DEPOT_PATH=$HOME/turbulence-at-fronts/.julia-trig
 
 julia -t 24 -- setup/setup-trig.jl
