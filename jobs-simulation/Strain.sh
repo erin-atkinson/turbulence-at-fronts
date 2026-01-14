dc89d46cc4018b98c5425751583195a3e2c9e7d3
Erin Atkinson <erin.atkinson@mail.utoronto.ca>
2026-01-14 13:13:41 -0500

    Larger resolution

--- jobs-simulation/Strain.sh
+++ jobs-simulation/Strain.sh
@@ -9,7 +9,7 @@ module load julia/1.10.10
 
 # Copy installation to RAM disk
 export RAM=/dev/shm/turbulence-at-fronts
-cp -r $HOME/.julia-trig $RAM
+cp -r $HOME/turbulence-at-fronts/.julia-trig $RAM
 
 # Launch from RAM disk
 export JULIA_DEPOT_PATH=$RAM/.julia-trig
@@ -17,10 +17,10 @@ export JULIA_SCRATCH_TRACK_ACCESS=0
 cd ~/turbulence-at-fronts
 
 output_path=$SCRATCH/turbulence-at-fronts/Strain
-run_time="8e5"
+run_time="4e5"
 f="1e-4"
 H="100"
-Nx=1024
+Nx=2048
 Ny=128
 Nz=256
 Ro="0.4"
@@ -30,6 +30,6 @@ Q="100"
 c="0.5"
 start_time="-4e5"
 save_time="1e3"
-s="1.05"
+s="1.018"
 
 julia -t 24 -- src-simulation/simulation.jl $output_path $run_time $f $H $Nx $Ny $Nz $Ro $Ri $alpha $Q $c $start_time $save_time $s "Trillium GPU"
